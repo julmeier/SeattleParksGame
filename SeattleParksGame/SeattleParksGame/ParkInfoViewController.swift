5dcbaa49cddb26ff95d55328391ebb865f1dad75
Julia <julia.meier@gmail.com>
2018-01-03 10:24:47 -0800

    created initial Swift project, created gitignore file in the main project folder

---
+++ SeattleParksGame/SeattleParksGame/ParkInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ParkInfoViewController.swift
+//  SeattleParksGame
+//
+//  Created by Julia Meier on 1/2/18.
+//  Copyright © 2018 Julia Meier. All rights reserved.
+//
+
+import UIKit
+
+class ParkInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
