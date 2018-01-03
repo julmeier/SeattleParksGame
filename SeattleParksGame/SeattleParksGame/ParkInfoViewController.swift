e93b0d968138eb70fe03d371dc4f58f6d8ae3e97
Julia <julia.meier@gmail.com>
2018-01-03 13:26:17 -0800

    connecting to Firebase. added Podfile and GoogleService-Info.plist. May have any issue with having files in wrong folders

--- SeattleParksGame/SeattleParksGame/ParkInfoViewController.swift
+++ SeattleParksGame/SeattleParksGame/ParkInfoViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ParkInfoViewController: UIViewController {
 
+    @IBOutlet weak var parkName: UILabel!
+    @IBOutlet weak var parkAddress: UILabel!
+    @IBOutlet weak var visitStatus: UILabel!
+    @IBOutlet weak var parkImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        parkName.text = "Magnuson Park"
+        parkImage.image = UIImage(named: "GasworksHD-landscape")
+        //or use Image Literal to choose image from Asset catalogue
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeVisitStatusPressed(_ sender: Any) {
+        print("changeVisitStatus button tapped")
+        
+    }
+    
+    //tutorial shows this version instead. Why no explicit return?
+//    @IBAction func changeVisitStatusPressed(_ sender: AnyObject) -> Void {
+//    }
+    
 }
