e8911f49b2c9a55121c51b4e0d6938ee6a6782ce
Julia <julia.meier@gmail.com>
2018-01-05 11:45:06 -0800

    added first batch of test data to Firebase database, including park pmaid and their visit status by userID

--- SeattleParksGame/SeattleParksGame/MapViewController.swift
+++ SeattleParksGame/SeattleParksGame/MapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 import MapKit
 import Foundation
-//import FirebaseDatabase
+import FirebaseDatabase
 
 struct ParkAddress: Codable {
     let pmaid: String
@@ -27,11 +27,15 @@ class  MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
-//    var ref:FIRDatabaseReference?
-    var pin:AnnotationPin!
+//    var ref: DatabaseReference!
+    var dbReference: DatabaseReference?
+    var pin: AnnotationPin!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //set up Firebase database reference variable
+        dbReference = Database.database().reference()
         
         let initialLocation = CLLocation(latitude: 47.6074717, longitude: -122.3352511)
         zoomMapOn(location: initialLocation)
@@ -40,41 +44,45 @@ class  MapViewController: UIViewController {
 //        print(myPark)
 //        print("myPark is printed above")
 
-        let samplePin1 = AnnotationPin(
-            title: "SAMPLE PIN 1",
-            subtitle: "Frogs Legs",
-            coordinate: CLLocationCoordinate2D(latitude: 47.6074717, longitude: -122.3352511)
-        )
-        mapView.addAnnotation(samplePin1)
+//        let samplePin1 = AnnotationPin(
+//            title: "SAMPLE PIN 1",
+//            subtitle: "Frogs Legs",
+//            coordinate: CLLocationCoordinate2D(latitude: 47.6074717, longitude: -122.3352511)
+//        )
+//        mapView.addAnnotation(samplePin1)
         
-        let samplePin2 = AnnotationPin(
-            title: "Sample PIN 2",
-            subtitle: "Mac & Cheese",
-            coordinate: CLLocationCoordinate2D(latitude: 47.620586, longitude: -122.302231)
-        )
-        mapView.addAnnotation(samplePin2)
+//        let samplePin2 = AnnotationPin(
+//            title: "Sample PIN 2",
+//            subtitle: "Mac & Cheese",
+//            coordinate: CLLocationCoordinate2D(latitude: 47.620586, longitude: -122.302231)
+//        )
+//        mapView.addAnnotation(samplePin2)
         
         let path = Bundle.main.path(forResource: "SeattleParksAddresses", ofType: "json")
         let url = URL(fileURLWithPath: path!)
         do {
             let data = try Data(contentsOf: url)
             //print(String(data: data, encoding: .utf8)!)
-            let dataString = String(data: data, encoding: .utf8)!
+//            let dataString = String(data: data, encoding: .utf8)!
 //            print(dataString)
-//            print("dataString is above")
+//            print("dataString is above (prints all park)")
             
             let decoder = JSONDecoder()
             let parks = try decoder.decode([ParkAddress].self, from: data)
             for park in parks {
-                print(park.name)
-                var long = (park.x_coord as NSString).doubleValue
-                var lat = (park.y_coord as NSString).doubleValue
-                var aPin = AnnotationPin(
+//                print(park.name)
+                let long = (park.x_coord as NSString).doubleValue
+                let lat = (park.y_coord as NSString).doubleValue
+                let aPin = AnnotationPin(
                     title: park.name,
                     subtitle: park.address,
                     coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long)
                 )
                 mapView.addAnnotation(aPin)
+                
+                //ONLY ONCE AT ACCOUNT CREATION: create and populate initial Firebase database for testUser1:
+//                dbReference?.child("users").child("testUser1").child("parkVisits").child(park.pmaid).setValue(false)
+                
                 
             }
 
@@ -96,10 +104,8 @@ class  MapViewController: UIViewController {
             
         }
         
-        //set firebase reference
-//        ref = FIRDatabase.database().reference()
         
-        //Retrieve the posts and listen for changes
+        //Retrieve the firebase data and listen for changes
         
     }
     
