c2407bb2854bac5c99f60d079a9ca26d84432bf0
Julia <julia.meier@gmail.com>
2018-01-07 16:36:19 -0800

    SUCCESS. Map now shows purple trees if the PMAID is not in the database, and green trees if the PMAID is present in the database

--- SeattleParksGame/SeattleParksGame/AnnotationPin.swift
+++ SeattleParksGame/SeattleParksGame/AnnotationPin.swift
@@ -15,11 +15,13 @@ class AnnotationPin: NSObject, MKAnnotation {
     var subtitle: String?
     var coordinate: CLLocationCoordinate2D
     var visitStatusInPin: Bool?
+    var imageName: String?
     
-    init(title: String, subtitle: String?, coordinate: CLLocationCoordinate2D) {
+    init(title: String, subtitle: String?, coordinate: CLLocationCoordinate2D, imageName: String?) {
         self.title = title
         self.subtitle = subtitle
         self.coordinate = coordinate
+        self.imageName = imageName
         
         super.init()
     }
--- SeattleParksGame/SeattleParksGame/MapViewController.swift
+++ SeattleParksGame/SeattleParksGame/MapViewController.swift
@@ -21,23 +21,29 @@ struct ParkAddress: Codable {
     let y_coord: String
 }
 
-
 class  MapViewController: UIViewController, MKMapViewDelegate {
-
-    @IBOutlet weak var mapView: MKMapView!
-    var redTree = MKPointAnnotation()
-    var greenTree = MKPointAnnotation()
     
-//    var ref: DatabaseReference!
+//    class CustomPointAnnotation: MKPointAnnotation {
+//        var imageName: String!
+//        //var visitStatus: Bool!
+//    }
+
+    //Mapping variables:
+    @IBOutlet weak var mapView: MKMapView!
+    var purpleTree: AnnotationPin!
+    var greenTree: AnnotationPin!
+    var pin: AnnotationPin!
+    
+    //Firebase database references:
     var dbReference: DatabaseReference?
     var databaseHandle:DatabaseHandle?
-    var pin: AnnotationPin!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //delegate needed for custom pin
-        mapView.delegate = self
+        self.mapView.delegate = self
         
         //this doesn't seem to be working:
         mapView.showsUserLocation = true
@@ -48,6 +54,7 @@ class  MapViewController: UIViewController, MKMapViewDelegate {
         let initialLocation = CLLocation(latitude: 47.6074717, longitude: -122.3352511)
         zoomMapOn(location: initialLocation)
         
+        //Testing mapping with addAnnotation works:
 //        let myPark = ParkAddress(pmaid: "111", name: "Pretend Park", address: "pretend address", zip_code: "98101", x_coord: "42.6075", y_coord: "-122.3353")
 //        print(myPark)
 //        print("myPark is printed above")
@@ -80,7 +87,7 @@ class  MapViewController: UIViewController, MKMapViewDelegate {
             let decoder = JSONDecoder()
             let parks = try decoder.decode([ParkAddress].self, from: data)
             for park in parks {
-//                print(park.name)
+                //print("Park name is: \(park.name)")
                 let long = (park.x_coord as NSString).doubleValue
                 let lat = (park.y_coord as NSString).doubleValue
                 
@@ -90,42 +97,57 @@ class  MapViewController: UIViewController, MKMapViewDelegate {
                 if snapshot.hasChild(park.pmaid) {
                     print("pmaid in the db:")
                     print(park.pmaid)
+                    self.greenTree = AnnotationPin(
+                        title: park.name,
+                        subtitle: "true",
+                        coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long),
+                        imageName: "green-cloud-tree-32.png"
+                    )
+                    self.mapView.addAnnotation(self.greenTree)
                 } else {
                     print("pmaid NOT in the db:")
                     print(park.pmaid)
+                    self.purpleTree = AnnotationPin(
+                        title: park.name,
+                        subtitle: "false",
+                        coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long),
+                        imageName: "purple-cloud-tree-32.png"
+                    )
+                    self.mapView.addAnnotation(self.purpleTree)
                 }
             })
                 
                 //dbReference?.child("users/testUser1/parkVisits/\(park.pmaid)").observe(.value, with: { (snapshot) in
-                dbReference?.child("users/testUser1/parkVisits/\(park.pmaid)").observeSingleEvent(of: .value, with: { (snapshot) in
-                    parkPMAID = snapshot.key
+//                dbReference?.child("users/testUser1/parkVisits/\(park.pmaid)").observeSingleEvent(of: .value, with: { (snapshot) in
+//                    parkPMAID = snapshot.key
                     //print(parkPMAID!)
-                    let storedVisitStatus = snapshot.value as? Bool
-                    let storedVisitStatusString = self.BoolToString(b: storedVisitStatus)
+//                    let storedVisitStatus = snapshot.value as? Bool
+//                    let storedVisitStatusString = self.BoolToString(b: storedVisitStatus)
                     //print(storedVisitStatusString)
                     //print("next")
                     
                     //LiNGERING QUESTION: should I actually make unqiue model instances? i.e. make a unique identifier instead of "aPin" (ex. aPin12345) so that I can pass that object around the application and it persists after teh map has been loaded?
-                
-                    let aPin = AnnotationPin(
-                        title: park.name,
-                        subtitle: storedVisitStatusString,
-                        coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long)
-                    )
-                    aPin.visitStatusInPin = storedVisitStatus
-                    if snapshot.hasChild(park.pmaid){
-                        //print(aPin.visitStatusInPin!)
-                        //print("This park is LISTED:")
-                        //print(park.pmaid)
-                    }
-                    else {
-                        //print("This park is NOT listed:")
-                        //print(park.pmaid)
-                        
-                    }
+       
+                    //COMMENTED OUT THE BELOW WHILE TRYING TO CUSTOMIZE PINS
+//                    let aPin = AnnotationPin(
+//                        title: park.name,
+//                        subtitle: storedVisitStatusString,
+//                        coordinate: CLLocationCoordinate2D(latitude: lat, longitude: long)
+//                    )
+//                    aPin.visitStatusInPin = storedVisitStatus
+//                    if snapshot.hasChild(park.pmaid){
+//                        //print(aPin.visitStatusInPin!)
+//                        //print("This park is LISTED:")
+//                        //print(park.pmaid)
+//                    }
+//                    else {
+//                        //print("This park is NOT listed:")
+//                        //print(park.pmaid)
+//
+//                    }
                     
-                    self.mapView.addAnnotation(aPin)
-                })
+                    //self.mapView.addAnnotation(aPin)
+//                })
                 
                 
                 //TO CREATE TEST DATA: create and populate initial Firebase database for testUser1:
@@ -170,34 +192,57 @@ class  MapViewController: UIViewController, MKMapViewDelegate {
 //        annotationView.canShowCallout = true
 //        return annotationView
 //    }
-    
+
+//This was working before trying to customize the pin image:
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        if annotation is MKUserLocation
+//        {
+//            return nil
+//        }
+//        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+//        if annotationView == nil{
+//            annotationView = MKAnnotationView(annotation: pin, reuseIdentifier: "pin")
+//            annotationView?.canShowCallout = true
+//        }else{
+//            annotationView?.annotation = annotation
+//        }
+//
+//        //ADD LOGIC HERE ON WHICH TREE TO APPLY BASED ON GOOGLEFIRE DATABASE?
+//        //Ex. if users.username.parkvisits.park#.value = true, then apply green tree
+//        annotationView?.image = UIImage(named: "green-cloud-tree-32")
+//        return annotationView
+//    }
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation
-        {
+        
+        if !(annotation is MKAnnotation) {
+            print("Not registered as MKAnnotation")
             return nil
         }
-        var annotationView = self.mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
-        if annotationView == nil{
-            annotationView = MKAnnotationView(annotation: pin, reuseIdentifier: "pin")
-            annotationView?.canShowCallout = true
-        }else{
-            annotationView?.annotation = annotation
-        }
         
-        //ADD LOGIC HERE ON WHICH TREE TO APPLY BASED ON GOOGLEFIRE DATABASE?
-        //Ex. if users.username.parkvisits.park#.value = true, then apply green tree
-        annotationView?.image = UIImage(named: "green-cloud-tree-32")
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "parkIdentifier")
+                if annotationView == nil{
+                    annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "parkIdentifier")
+                    annotationView?.canShowCallout = true
+                }
+                else
+                {
+                    annotationView?.annotation = annotation
+                }
+        let cpa = annotation as! AnnotationPin
+        annotationView!.image = UIImage(named: cpa.imageName!)
         return annotationView
+        
     }
     
     func BoolToString(b: Bool?)->String { return b?.description ?? "<None>"}
     
     func mapView(_ mapView: MKMapView,
                  didSelect view: MKAnnotationView) {
-        
     }
     
     private let regionRadius: CLLocationDistance = 4000 //1km = 1000
+
     func zoomMapOn(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 3.0, regionRadius * 3.0)
         mapView.setRegion(coordinateRegion, animated:true)
