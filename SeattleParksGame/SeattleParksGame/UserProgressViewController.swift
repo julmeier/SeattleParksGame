050353994e30c569448e94a436a19ae00eaff4ac
Julia <julia.meier@gmail.com>
2018-01-13 00:50:29 -0800

    Created zipcode-neighborhood dictionary as variable in the userProgressViewController. Now figuring out if I should make another API call or pass data from the mapview to the userprogress VCs.

--- SeattleParksGame/SeattleParksGame/UserProgressViewController.swift
+++ SeattleParksGame/SeattleParksGame/UserProgressViewController.swift
@@ -7,8 +7,40 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+
 
 class UserProgressViewController: UIViewController {
+    
+    //badges dictionary
+    let badges = ["98101": "Downtown",
+        "98102": "Capitol Hill",
+        "98103": "Greenwood-Fremont-Greenlake",
+        "98104": "International District-Pioneer Square",
+        "98105": "University District-Laurelhurst",
+        "98106": "Delridge",
+        "98107": "Ballard",
+        "98108": "Beacon Hill",
+        "98109": "South Lake Union",
+        "98112": "Madison Valley",
+        "98115": "Northeast",
+        "98116": "West Seattle",
+        "98117": "Sunset Hill-Whittier Heights",
+        "98118": "Columbia City",
+        "98119": "Queen Anne",
+        "98121": "Belltown",
+        "98122": "First Hill-Madrona",
+        "98125": "Northgate",
+        "98126": "High Point",
+        "98133": "Bitterlake",
+        "98136": "Fauntleroy",
+        "98144": "Mount Baker",
+        "98146": "Mount View-Burien",
+        "98177": "Richmond Beach",
+        "98178": "Bryn Mawr-Skyway",
+        "98199": "Magnolia"]
+    let parksByZip = [String: [String]]()
     
     //storyboard variables
     @IBOutlet weak var numberVisited: UILabel!
@@ -19,26 +51,28 @@ class UserProgressViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        numberVisited.text = "Hello world"
 
-        // Do any additional setup after loading the view.
+        //set up Firebase database reference variable
+        dbReference = Database.database().reference()
+        
+        //display the number of parks visited:
+        dbReference?.child("users/testUser1/parkVisits").observeSingleEvent(of: .value, with: { (snapshot) in
+            //print("snapshot.childrenCount:")
+            //print(snapshot.childrenCount)
+            let numberVisitedFromDB = snapshot.childrenCount
+            self.numberVisited.text = String(numberVisitedFromDB)
+            
+        })
+        
+        //diplay the entire collction of possible badges user can earn
+        //icons distinguish whether user has earned them yet or not
+        //may show the number of parks needed to earn each badge (ex. achieved 6/7 parks - only 1 to go!)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
